a6dda576df3933a51394279d9179c425b3035f8a
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-08-26 20:31:27 -0300

    feat: cart provider

---
+++ src/providers/cartProvider/index.tsx
@@ -0,0 +1,34 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface Product {}
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+interface CartProviderData {
+  cartList: Product[];
+  addProduct: (product: Product) => void;
+  removeProduct: (product: Product) => void;
+}
+
+const CartContext = createContext<CartProviderData>({} as CartProviderData);
+
+export const CartProvider = ({ children }: CartProviderProps) => {
+  const [cartList, setCartList] = useState<Product[]>([]);
+
+  const addProduct = (product: Product) => {
+    setCartList([...cartList, product]);
+  };
+
+  const removeProduct = (product: Product) => {
+    const newList = cartList.filter((element) => element.name !== product.name);
+    setCartList(newList);
+  };
+
+  return (
+    <CartContext.Provider value={{ cartList, addProduct, removeProduct }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
